1b924302a0d4df4ea8a9827b53a7c21acbde3747
b0n541 <j@nschaefer.net>
2021-12-08 17:32:30 +0100

    Add complete solution for day 4

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,38 +1,52 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var drawnNumbers = input[0].split(",").map { it.toInt() }
-        println(drawnNumbers)
-
-        var bingoBoards = getBingoBoards(input)
-
-        for (index in drawnNumbers.indices) {//drawnNumbers.indices) {
-            val nextNumber = drawnNumbers[index]
-            println("Next number $nextNumber")
-            bingoBoards.forEach {
-                val isBingo = it.markNumber(nextNumber)
-                if (isBingo) {
-                    println("BINGO!")
-                    println(it)
-                    return it.sumOfUnmarkedNumbers() * nextNumber
-                }
-            }
-        }
-
-        return -1
+        return playBingo(input, GameEndRule.FIRST_BOARD)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return playBingo(input, GameEndRule.LAST_BOARD)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    //check(part1(testInput) == 4512)
+    check(part1(testInput) == 4512)
     check(part2(testInput) == 1924)
 
     val input = readInput("Day04")
-    //println(part1(input))
+    println(part1(input))
     println(part2(input))
+}
+
+fun getDrawnNumbers(input: List<String>) = input[0].split(",").map { it.toInt() }
+
+fun playBingo(input: List<String>, gameEndRule: GameEndRule): Int {
+    var drawnNumbers = getDrawnNumbers(input)
+    println(drawnNumbers)
+
+    var bingoBoards = getBingoBoards(input)
+
+    for (index in drawnNumbers.indices) {
+        val nextNumber = drawnNumbers[index]
+        println("Next number $nextNumber")
+        bingoBoards.forEach {
+            val isBingo = it.markNumber(nextNumber)
+            if (isBingo) {
+                println("BINGO!")
+                println(it)
+                when (gameEndRule) {
+                    GameEndRule.FIRST_BOARD -> return it.sumOfUnmarkedNumbers() * nextNumber
+                    GameEndRule.LAST_BOARD -> {
+                        if (bingoBoards.size == 1) {
+                            return it.sumOfUnmarkedNumbers() * nextNumber
+                        }
+                    }
+                }
+            }
+        }
+        bingoBoards.removeIf { it.isBingo }
+    }
+
+    return -1
 }
 
 fun getBingoBoards(input: List<String>): MutableList<BingoBoard> {
@@ -48,6 +62,8 @@ fun getBingoBoards(input: List<String>): MutableList<BingoBoard> {
 
 class BingoBoard(private val numbers: List<MutableList<Int>>) {
 
+    var isBingo = false
+
     fun markNumber(number: Int): Boolean {
         for (row in numbers.indices) {
             for (column in numbers[row].indices) {
@@ -61,8 +77,11 @@ class BingoBoard(private val numbers: List<MutableList<Int>>) {
     }
 
     private fun checkBingo(row: Int, column: Int): Boolean {
-        return numbers[row].all { it == -1 }
-                || numbers.map { it[column] }.all { it == -1 }
+        if (numbers[row].all { it == -1 }
+            || numbers.map { it[column] }.all { it == -1 }) {
+            isBingo = true
+        }
+        return isBingo
     }
 
     fun sumOfUnmarkedNumbers(): Int {
@@ -83,4 +102,9 @@ fun BingoBoard(input: List<String>): BingoBoard {
             .toMutableList()
     }
     return BingoBoard(numbers)
+}
+
+enum class GameEndRule {
+    FIRST_BOARD,
+    LAST_BOARD
 }
